c2a2d086189b11af5ef545a403a149141690c1c4
test@gmail.com <test@gmail.com>
2022-02-24 09:47:33 +0000

    init commit

---
+++ src/main/java/com/soj/designpattern/singleton/Main.java
@@ -0,0 +1,12 @@
+package com.soj.designpattern.singleton;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    static final Logger logger= LogManager.getLogger(Main.class.getName());
+    public static void main(String[] args){
+        logger.info("hello");
+
+    }
+}
