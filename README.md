d7563d88346f4e8bb3a4197f6ddd751f07613540
test@gmail.com <test@gmail.com>
2022-02-24 21:01:34 +0000

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SingletonPattern
+
+
+JunitTest:
+      There is JUnit Test also present for all Single Responsiblity Principle  and test coverage are 100%
+successful by jacoco reports.
+     
+SONARQUBE 
+
+gradle command to publish code to sonarqube
+
+
+gradle sonarqube 
+  -Dsonar.projectKey=SingletonPattern 
+  -Dsonar.host.url=http://localhost:9000 
+  -Dsonar.login=4fdbbf97714f18bf10b6a8ef1c077d782ddf15f1
--- src/main/java/com/soj/designpattern/singleton/GetTheTiles.java
+++ src/main/java/com/soj/designpattern/singleton/GetTheTiles.java
@@ -8,7 +8,7 @@ public class GetTheTiles implements Runnable {
     static final Logger logger= LogManager.getLogger(GetTheTiles.class.getName());
     @Override
     public void run() {
-        Singleton newInstance=Singleton.getInstance();
+        ScrabbleSingleton newInstance= ScrabbleSingleton.getInstance();
         List<String> playerOneTiles=newInstance.getTiles(7);
         logger.debug("Instance ID: {}",System.identityHashCode(newInstance));
         logger.debug(newInstance.getLetterList());
